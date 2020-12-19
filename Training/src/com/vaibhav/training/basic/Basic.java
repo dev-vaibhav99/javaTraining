6c3caae9655cac202339f7e5655d40d533d89b75
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2020-12-19 19:48:48 +0530

    Training Project added

---
+++ Training/src/com/vaibhav/training/basic/Basic.java
@@ -0,0 +1,8 @@
+package com.vaibhav.training.basic;
+
+public class Basic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Training/src/com/vaibhav/training/basic/Variables.java
@@ -0,0 +1,34 @@
+package com.vaibhav.training.basic;
+
+public class Variables {
+    public static void main(String[] args) {
+        /* A legal variable name should follow some rules:
+        * a variable name can contain letters, numbers and underscore
+        * it should not start with a number
+        * special symbols are not allowed
+        * only underscore is allowed in a variable name
+        * the variable name should always be relative and meaningful */
+
+        byte verySmallNumber = 1; // byte date type have very small range (1 byte)
+
+        short smallNumber = 3343; // short data type have slightly big range than byte (2 bytes)
+
+        int num; // declaring a variable with the data type.. int data type supports only whole numbers (4 bytes)
+        num = 10000;// initiating the variable with some value
+        num+=num;  // using the variable in an expression
+        System.out.println(num); //using the variable as we need them
+        long bigNumber =99334343; // long data type supports whole values of large range (8 bytes)
+
+        float decimal = 3.14f; // float data type supports decimal values (4 bytes)
+
+        double bigDecimal = 99.84543; // double data type have large range of decimal values (8 bytes)
+
+        char letter = 'a'; // char stores only single letters or number.. should be written in single quotes
+        String word = "Hi, I hope you have understood variables and data types clearly";
+        // this is a string data and in java String is a class..
+        // String should be written in double quotes
+
+        System.out.println(word);
+
+    }
+}
