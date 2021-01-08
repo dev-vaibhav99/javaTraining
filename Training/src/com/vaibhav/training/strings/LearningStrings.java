a024bcbe5a6aebb94e9692c592da9a5ef38337f0
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2021-01-08 20:19:22 +0530

    Written some rules and functions in the form of comments to remember in future

---
+++ Training/src/com/vaibhav/training/strings/LearningStrings.java
@@ -0,0 +1,16 @@
+package com.vaibhav.training.strings;
+
+public class LearningStrings {
+    /* String is a sequence of characters
+    *  Strings should always be enclosed in "Double Quotes"
+    *  Each character in string have an index
+    *  Index starts from 0 till length-1
+    *  charAt(index) functions returns character at given index
+    *  str.length() returns the length of string
+    *  substring(start,end-1) takes starting_point(compulsory,from given index) and ending_point(optional,given index-1) as arguments
+    *  str.equals(string) checks if given strings are equal or not (case sensitive)
+    *  str.equalsIgnoreCase(string) is not case sensitive and checks if both strings are equal
+    *  indexOf(string) returns index of given string (from starting)
+    *  lastIndexOf(string) returns index of given string (from last) */
+    String str = "This is a string...";
+}
