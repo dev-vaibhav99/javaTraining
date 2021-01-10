a193a849ead4e215c030c5d8f1a901a4e81188fa
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2021-01-10 21:57:45 +0530

    Learnt to use some more string functions

--- Training/src/com/vaibhav/training/strings/LearningStrings.java
+++ Training/src/com/vaibhav/training/strings/LearningStrings.java
@@ -12,5 +12,42 @@ public class LearningStrings {
     *  str.equalsIgnoreCase(string) is not case sensitive and checks if both strings are equal
     *  indexOf(string) returns index of given string (from starting)
     *  lastIndexOf(string) returns index of given string (from last) */
-    String str = "This is a string...";
+
+    public static void main(String[] args) {
+        String str = "This is a string...";
+        String hello = "Hello";
+
+        int lengthOfString = str.length();
+        System.out.println("Length of the string is: "+lengthOfString);
+
+        char charAtIndex = str.charAt(6);
+        System.out.println("Character at index 6 is:"+charAtIndex);
+
+        String subString = str.substring(9,16);
+        System.out.println("Substring from index 9 to 16 is:"+subString);
+
+        String subStringTillEnd = str.substring(9);
+        System.out.println("Substring from index 9 till end:"+subStringTillEnd);
+
+        int indexFromStarting = str.indexOf("is");
+        System.out.println("The first occurrence of string word on index: "+indexFromStarting);
+
+        int indexFromLast = str.lastIndexOf("is");
+        System.out.println("The last occurrence of string word on index: "+indexFromLast);
+        //To check if two strings are equal we use str.equals(string) method [not == for strings]
+        //equals() is case-sensitive
+        if(hello.equals("hello")){
+            System.out.println("Matching case Hello");
+        }
+        else{
+            System.out.println("Non matching hello");
+        }
+
+        if(hello.equalsIgnoreCase("hello")){
+            System.out.println("Case is ignored");
+        }
+        else{
+            System.out.println("This is different spelling");
+        }
+    }
 }
